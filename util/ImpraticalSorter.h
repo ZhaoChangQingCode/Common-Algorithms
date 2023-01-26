733e206d446c781c32a71521380da15b7f2c186b
昭裳卿 <120012529+ZhaoChangQingCode@users.noreply.github.com>
2023-01-26 18:04:09 +0800

    Add files via upload

--- util/ImpraticalSorter.h
+++ util/ImpraticalSorter.h
@@ -3,14 +3,14 @@
 using size_type = unsigned int;
 
 template<class T> bool isSorted(T* a, size_type low, size_type high) {
-	bool sorted;
-	size_type i = low - 1;
+    bool sorted;
+    size_type i = low - 1;
 
-	do {
-		sorted = (a[++i] < a[i + 1]);
-	} while (i + 1 < high && sorted);
+    do {
+        sorted = (a[++i] < a[i + 1]);
+    } while (i + 1 < high && sorted);
 
-	return i == high - low;
+    return i == high - low;
 }
 
 /**
@@ -19,11 +19,11 @@ template<class T> bool isSorted(T* a, size_type low, size_type high) {
  * 虽然没什么用，但故事大有来头。
  */
 template<class T> void bogoSort(T* a, size_type low, size_type high) {
-	size_type size = high - low + 1;
+    size_type size = high - low + 1;
 
-	while (!isSorted(a, low, high)) {
-		for (size_type i = low; i <= high; i++) {
-			swap(a[i], a[rand() % size]); // shuffle
-		}
-	}
+    while (!isSorted(a, low, high)) {
+        for (size_type i = low; i <= high; i++) {
+            swap(a[i], a[rand() % size]); // shuffle
+        }
+    }
 }
