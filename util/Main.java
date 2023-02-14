fd122970f60cc7b7e8f295eee3478c2cb29dc6a8
昭裳卿 <120012529+ZhaoChangQingCode@users.noreply.github.com>
2023-02-14 19:58:58 +0800

    Add files via upload

---
+++ util/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		System.out.println("Hello, World!");
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+}
