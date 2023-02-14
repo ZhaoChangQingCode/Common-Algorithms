f9fc2e2a536ffdce1c5e16431549cda624a41a97
昭裳卿 <120012529+ZhaoChangQingCode@users.noreply.github.com>
2023-02-14 21:04:20 +0800

    Delete Main.java

--- util/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		System.out.println("Hello, World!");
-		long end = System.currentTimeMillis();
-		System.out.println(end - start);
-	}
-}
