749144b15f2636a70ca2500a41b16e510f6e6772
昭裳卿 <120012529+ZhaoChangQingCode@users.noreply.github.com>
2023-02-14 17:40:05 +0800

    Add files via upload

--- util/Sorter.h
+++ util/Sorter.h
@@ -1,22 +1,15 @@
 /**
- * @author 昭裳卿@QQ:956995844
+ * 版权归昭裳卿所有（2023），保留所有权。请勿更改或删除此注释。
+ * 
+ * 此代码为免费参阅，可以任意修改、传播，希望此举措可以有所帮助。
+ * 如果您发现了一个错误，请联系我：956995844@QQ
  */
-#ifndef __cpluslpus
-    #error "C++ supported only!"
-#endif
-#include <exception>
-#include <map>
-#include <unordered_map>
-#include <math.h>
-#include <thread>
-#include <vector>
-#include <typeinfo>
-#include <cassert>
+#include <cmath>
 
 typedef unsigned int size_type;
 
-using COMB_SORT_SHRINK = 1.3F;
-using COMB_SORT_THRESHOLD = 64;
+using COMBSORT_SHRINK = 1.3F;
+using COMBSORT_THRESHOLD = 64;
 using QUICKSORT_THRESHOLD = 512;
 
 /**
@@ -141,7 +134,7 @@ template<class T> void combSort(T* a, size_type low, size_type high) {
     size_type i = low;
     bool swapped;
     do {
-        gap = (size_type) (gap / COMB_SORT_SHRINK);
+        gap = (size_type) (gap / COMBSORT_SHRINK);
         for (size_type i = low; i + gap <= high; i++) {
             if (swapped = (a[i] > a[i + gap])) {
                 swap(a[i], a[i + gap]);
@@ -151,36 +144,9 @@ template<class T> void combSort(T* a, size_type low, size_type high) {
 }
 
 /**
- * 计数排序（键值对映射的）
- */
-template<class T> void mappedCountingSort(T* a, size_type low, size_type high) {
-    std::ordered_map<T,size_type> count;
-    T min = a[low], max = a[high];
-    // 计算最大最小值
-    // for (size_type i = low - 1; i < high;
-    //     min = min(min, a[++i]), max = max(max, a[i])
-    // );
-    // 初始化映射容器
-    for (size_type i = low - 1; i < high; count[++i] = 0);
-    // 计数
-    for (size_type i = low - 1; i < high; ++count[a[++i]]);
-
-    for (std::map<T,size_type>::iterator i = count.begin; i != count.end; i++) {
-        while (*++i->second == 0);
-
-        T value = *i->first;
-        size_type c = *i->second;
-
-        do {
-            a[++low] = value;
-        } while (--c > 0);
-    }
-}
-
-/**
  * 计数排序（索引）
  */
-template<class T> void indexedCountingSort(T* a, size_type low, size_type high) {
+template<class T> void countingSort(T* a, size_type low, size_type high) {
     T min = a[low], max = a[low];
 
     for (size_type i = low - 1; i < high;
@@ -235,6 +201,9 @@ template<class T> void indexedCountingSort(T* a, size_type low, size_type high) 
     }
 }
 
+/**
+ * PIN 插入
+ */
 template<class T> void pinInsertionSort(T* a, size_type low, size_type high) {
     T pin = a[high];
 
@@ -293,15 +262,6 @@ template<class T> void pinInsertionSort(T* a, size_type low, size_type high) {
     }
 }
 
-
-template<class T> void countingSort(T* a, size_type low, size_type high) {
-    if (sizeof(a[0]) <= 4 && typeid(T) != typeid(float)) { // 整数类型（char、short、int、long）？
-        indexedCountingSort(a, low, high);
-    } else {
-        mappedCountingSort(a, low, high);
-    }
-}
-
 /**
  * 选择排序
  */
@@ -324,142 +284,10 @@ template<class T> void biSelectionSort(T* a, size_type low, size_type high) {
 
     while (low < high) {
         T min = a[low], max = a[high];
-        // 寻找最大、最小值
-        for (size_type i = low; i < high; i++) {
-            T k = a[i];
 
-            if (k < min) {
-                min = k;
-            } else if (k > max) {
-                max = k;
-            }
-        }
-        swap(min, a[low++]);  // 最小值放最左边，前面的元素标记为已排序
-        swap(max, a[high--]); // 最大值放最右边，后面的元素标记为已排序
+        for (size_type j = low; j < high;
+            min = min(min, a[++j]), max = max(max, a[j])
+        );
+        swap(min, a[low++]); swap(max, a[high--]);
     }
-
-    if (size & 1 == 1) {
-        insertionSort(a, --min, ++max);
-    }
-}
-
-/**
- * 二分法插入排序
- *
- * 插入排序的改良版，用二分法查找减少比较次数。
- */
-template<class T> void binaryInsertionSort(T* a, size_type low, size_type high) {
-    size_type left = low, right = high;
-    for (size_type i = low; i < high; i++) {
-        T k = a[i];
-
-        while (left <= right) { // binary search for insertion position
-            size_type mid = (left + right) >> 1;
-            T mid_val = a[mid];
-
-            if (mid_val > k) {
-                right = mid - 1;
-            } else if (mid_val < k) {
-                left = mid + 1;
-            }
-        }
-
-        for (size_type j = i - 1; j-- > low; a[j + 1] = a[j]);
-        a[left] = k;
-    }
-}
-
-template<class T> void radixSort(T* a, size_type low, size_type high) {}
-
-template<class T> void dualPivotQuicksort(T* a, size_type low, size_type high) {}
-
-template<class T> void johnSort(T* a, size_type low, size_type high) {
-    
-}
-
-template<class T> void sort(T* a, size_type low, size_type high) {
-    size_type size = high - low + 1;
-
-    /**
-     * 对小容量数组使用希尔排序
-     */
-    if (size <= COMB_SORT_THRESHOLD) {
-        shellSort(a, low, high);
-        return;
-    }
-
-    /**
-     * 对中等容量数组使用梳排序
-     */
-    if (size <= QUICKSORT_THRESHOLD) {
-        combSort(a, low, high);
-        return;
-    }
-
-    /**
-     * 对大容量*整数*数组使用基数排序
-     */
-    if (size && sizeof(T) <= 4 && typeid(T) != typeid(float)) {
-        radixSort(a, low, high);
-        return;
-    }
-
-    /**
-     * 使用双轴快速排序
-     */
-    dualPivotQuicksort(a, low, high);
-}
-
-template<class T> bool a(T* a, size_type low, size_type high) {
-    for (size_type k = low + 1; k < high; ) {
-
-        if (a[k - 1] < a[k]) {
-
-            // 确认递增序列
-            while (++k < high && a[k - 1] <= a[k]);
-
-        } else if (a[k - 1] > a[k]) {
-
-            // 确认递减序列
-            while (++k < high && a[k - 1] >= a[k]);
-
-            // 倒转
-            for (size_type i = high - 1, j = k; ++i < --j && a[i] > a[j]) {
-                swap(a[i], a[j]);
-            }
-        } else { // 确认连相等的序列
-            for (T ak = a[k]; ++k < high && ak == a[k];);
-
-            if (k < high) {
-                continue;
-            }
-        }
-
-        if (k == high) {
-            return true;
-        }
-
-        if (k - low < MIN_RUN_SIZE) {
-            return false;
-        }
-    }
-}
-
-template<class T> void pushDown() {
-    size_type k;
-
-    do {
-        k = p << 1 - low + 2;
-
-        if (k > high) {
-            break; // 断行
-        }
-        if (k == high || a[k] < a[k - 1]) {
-            --k;
-        }
-        a[p] = a[p = k];
-
-    } while (a[k] > value);
-
-    a[p] = value;
 }
